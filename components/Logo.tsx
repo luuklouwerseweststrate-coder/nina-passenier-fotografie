24778e3562756086e6a3c106db76e91dcb95054a
luuklouwerseweststrate-coder <luuklouwerse.weststrate@gmail.com>
2026-04-20 16:02:43 +0200

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,13 +7,12 @@ type Props = {
 
 export default function Logo({ className = "", priority = false }: Props) {
   return (
-    <Image
-      src="/logo.png"
-      alt="Nina Passenier Fotografie"
-      width={1200}
-      height={440}
-      priority={priority}
-      className={className}
-    />
-  );
-}
+  <Image
+    src="/logo.png"
+    alt="Nina Passenier Fotografie"
+    width={1200}
+    height={440}
+    priority={priority}
+    className={`h-14 w-auto ${className}`}
+  />
+);
